d0065470b654ec893de388587f66e6f641379b8c
Sergey Tyumentsev <samposebe@ngs.ru>
2022-09-29 01:55:03 +0800

    :задача 56_2

--- Семинар8_Задача56/Program.cs
+++ Семинар8_Задача56/Program.cs
@@ -6,42 +6,42 @@ Console.WriteLine("Задайте параметры двумерного мас
 Console.Write("Введите количество строк m: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов n: ");
-int n = int.Parse (Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
 Console.Write("Задайте min значение массива: ");
-int min = int.Parse (Console.ReadLine());
+int min = int.Parse(Console.ReadLine());
 Console.Write("Задайте max значение массива: ");
-int max = int.Parse (Console.ReadLine());
+int max = int.Parse(Console.ReadLine());
 
 // 2. Объявляем двумерный массив.
-int[,] matrix = new int[m,n];
+int[,] matrix = new int[m, n];
 
 // 3. Инициализируем массив с помощью генератора случайных чисел.
-void FillArray (int[,] matrix)
+void FillArray(int[,] matrix)
 {
-    for (int i=0; i<matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-       for (int j=0; j<matrix.GetLength(1); j++)
-       {
-       matrix[i,j] = new Random().Next(min,max+1);
-       }
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
     }
 }
 
 // 5. Печатаем матрицу.
-void PrintArray (int[,] matrix)
+void PrintArray(int[,] matrix)
 {
-    for (int i=0; i<matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-    for (int j=0; j<matrix.GetLength(1); j++)
-    {
-    Console.Write ($"{matrix[i,j]}{" "}");
-    }
-    Console.WriteLine();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}{" "}");
+        }
+        Console.WriteLine();
     }
 }
 
 // 6. Находим строку с наименьшей суммой элементов. Выводим номер строки с наименьшей суммой элементов в терминал.
- void StringWithTheSmallestSumOfElements (int[,] matrix)
+void StringWithTheSmallestSumOfElements(int[,] matrix)
 {
     int index = 0;
     int minsumma = 0;
@@ -50,32 +50,26 @@ void PrintArray (int[,] matrix)
         int summa = 0;
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-        summa+=matrix[i,j];
+            summa += matrix[i, j];
         }
         if (i == 0)
         {
-        minsumma = summa;
+            minsumma = summa;
         }
         else if (summa < minsumma)
         {
-        minsumma = summa;
-        index = i;
+            minsumma = summa;
+            index = i;
         }
     }
-    string line = string.Empty;
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        line += matrix[index, j] + " ";
-    }
-    Console.Write($"Строка с наименьшей суммой элементов: {index+1}.");
+    Console.Write($"Строка с наименьшей суммой элементов: {index + 1}."); // index+1 - учитываем, что нумерация индекса начинается с 0.
     Console.WriteLine();
-    
 }
 
 // 7. Вызываем методы.
-FillArray (matrix);
+FillArray(matrix);
 Console.Write("Задан массив:");
 Console.WriteLine();
-PrintArray (matrix);
-StringWithTheSmallestSumOfElements (matrix);
+PrintArray(matrix);
+StringWithTheSmallestSumOfElements(matrix);
 Console.WriteLine();
