a72f59d87ba7fd3569f3f6c3e1a3afcefdff894d
Sergey Tyumentsev <samposebe@ngs.ru>
2022-10-02 00:41:36 +0800

    :задача 58

--- Семинар8_Задача58/Program.cs
+++ Семинар8_Задача58/Program.cs
@@ -3,10 +3,8 @@
 // 1. Задаем параметры первой матрицы.
 Console.WriteLine("Задайте параметры первой матрицы.");
 Console.Write("Введите количество строк m1: ");
-//int m1 = int.Parse(Console.ReadLine());
 int m1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов n1: ");
-//int n1 = int.Parse(Console.ReadLine());
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Задайте min1 значение массива: ");
 int min1 = int.Parse(Console.ReadLine());
@@ -16,27 +14,17 @@ int max1 = int.Parse(Console.ReadLine());
 // 2. Задаем параметры второй матрицы.
 Console.WriteLine("Задайте параметры второй матрицы.");
 Console.Write("Введите количество строк m2: ");
-//int m2 = int.Parse(Console.ReadLine());
 int m2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов n2: ");
-//int n2 = int.Parse(Console.ReadLine());
 int n2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Задайте min2 значение массива: ");
 int min2 = int.Parse(Console.ReadLine());
 Console.Write("Задайте max2 значение массива: ");
 int max2 = int.Parse(Console.ReadLine());
 
-// Проверяем матрицы на совместимость.
-//if (n1 != m2)
-//{
-//Console.Write("Матрицы не согласованы."); 
-//}
-
 // 3. Объявляем массивы.
 int[,] matrix1 = new int[m1, n1];
 int[,] matrix2 = new int[m2, n2];
-//int[,] Multiplication = (matrix1, matrix2);
-
 
 // 4. Инициализация матрицы с помощью генератора случайных чисел.
 void FillArray(int[,] matrix)
@@ -63,22 +51,22 @@ void PrintArray(int[,] matrix)
     }
 }
 
-// 6. 
+// 6. Проверка матриц на совместимость и их перемножение.
 static int[,] MatrixProduct(int[,] matr1, int[,] matr2)
 {
     if (matr1.GetLength(1) != matr2.GetLength(0)) throw new Exception("Матрицы не согласованы.");
-    int[,] matrixRez = new int[matr1.GetLength(0), matr2.GetLength(1)];
+    int[,] matrixRezult = new int[matr1.GetLength(0), matr2.GetLength(1)];
     for (int i = 0; i < matr1.GetLength(0); i++)
     {
         for (int j = 0; j < matr2.GetLength(1); j++)
         {
             for (int k = 0; k < matr2.GetLength(0); k++)
             {
-                matrixRez[i, j] += matr1[i, k] * matr2[k, j];
+                matrixRezult[i, j] += matr1[i, k] * matr2[k, j];
             }
         }
     }
-    return matrixRez;
+    return matrixRezult;
 }
 
 // 7. Вызываем методы.
@@ -91,8 +79,8 @@ Console.WriteLine();
 Console.WriteLine("Матрица 2:");
 PrintArray(matrix2);
 Console.WriteLine();
-int[,] matrixRez = MatrixProduct (matrix1, matrix2);
+int[,] matrixRezult = MatrixProduct(matrix1, matrix2);
 Console.Write("Результирующая матрица:");
 Console.WriteLine();
-PrintArray(matrixRez);
+PrintArray(matrixRezult);
 Console.WriteLine();
